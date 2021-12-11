30dbe90cda689c0bb7b9e326b5cd6a08c76f74a1
rob <brubachr@gmail.com>
2021-12-11 14:27:06 -0700

    added style to function btn in index.js

--- coffeeProjext/index.js
+++ coffeeProjext/index.js
@@ -5,3 +5,23 @@ hamburger.addEventListener("click", () =>{
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 })
+
+
+function btn(){
+  let btnS = document.getElementById("target");
+  btnS.innerHTML = "Order received";
+  btnS.style.color = "white";
+  btnS.style.fontFamily = "cursive";
+}
+
+document.getElementById("btnOne").onclick = function(){
+  btn();
+}
+
+document.getElementById("btnTwo").onclick = function(){
+  btn();
+}
+
+document.getElementById("btnThree").onclick = function(){
+  btn();
+}
