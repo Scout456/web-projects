7a04a923bbdf2c8a65c4e0a257718b5660185fd9
rob <brubachr@gmail.com>
2022-01-08 11:12:07 -0700

    added querySelector .hamburger-menuTwo and .navbarTwo in dog.js

---
+++ dog-Site/dog.js
@@ -0,0 +1,5 @@
+const iconMenu = document.querySelector(".hamburger-menuTwo");
+const navbarTwo = document.querySelector(".navbarTwo");
+iconMenu.addEventListener("click", () => {
+  navbarTwo.classList.toggle("changeTwo")
+})
