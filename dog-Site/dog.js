75c92c9d08675ff7ce47e5d63f3e093f2c7b53d1
rob <brubachr@gmail.com>
2022-01-22 13:44:51 -0700

    added intersectional observer with queryselector all

--- dog-Site/dog.js
+++ dog-Site/dog.js
@@ -133,19 +133,45 @@ function handleIntersect(entries, observer) {
 
 /* Intersecrion Observer---------------------------------------------------------------------*/
 
-const section = document.querySelector(".section-2");
-const imgContent = document.querySelector(".img");
+"use strict";
+const allSections = document.querySelectorAll("section");
+console.log(allSections);
 
-const objOptions = {
+const options = {
   root: null,
   threshold: 0.3,
-  rootMargin: "-100px",
 };
 
-const sectionObserver = new IntersectionObserver(callBackFunction, objOptions);
-sectionObserver.observe(section);
+const sectionObserver = new IntersectionObserver(callback, options);
 
-function callBackFunction(entries) {
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+});
+
+document.querySelectorAll(".left-column").forEach((column) => {
+  column.classList.add("hidden-left");
+});
+document.querySelectorAll(".right-column").forEach((column) => {
+  column.classList.add("hidden-right");
+});
+
+function callback(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  //   get the class name of current section that is in view
+  const curSectionsName = entry.target.getAttribute("class");
+  //   Now select that specific target section
+  const curSection = document.querySelector(`.${curSectionsName}`);
+  curSection.lastElementChild.firstElementChild.classList.remove("hidden-left");
+  curSection.lastElementChild.lastElementChild.classList.remove("hidden-right");
+
+  observer.unobserve(entry.target);
+}
+
+
+
+/*function callBackFunction(entries) {
   const [entry] = entries;
   console.log(entry);
   if (entry.isIntersecting) {
@@ -153,7 +179,11 @@ function callBackFunction(entries) {
   } else {
     imgContent.classList.add("hidden");
   }
-}
+}*/
+
+
+
+
 
 
 
